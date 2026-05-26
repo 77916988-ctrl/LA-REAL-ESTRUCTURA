8cfddfd572a038904d6d50602e31d7a7536180c9
77916988-ctrl <77916988@continental.edu.pe>
2026-05-25 19:19:26 -0500

    TRABAJO GRUPAL ESTRUCTURA DE DATOS
    includes y estructuras de datos base

--- trabajo grupal.cpp
+++ trabajo grupal.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+struct Alumno {
+    int id;
+    string nombre;
+    float c1, ep, c2, ef;
+    float promedio;
+    string estado;
+};
 
+struct NodoLista { Alumno info; NodoLista* siguiente; };
+struct NodoCola  { Alumno info; NodoCola* siguiente; };
+struct NodoPila  { string accion; NodoPila* siguiente; };
 int main() {
 
     return 0;
