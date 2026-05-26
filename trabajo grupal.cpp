ca31b65bafa5f3ad2856b3a11f426ddd9b53f154
77489075-sys <77489075@continental.edu.pe>
2026-05-26 08:02:50 -0500

    Implement student registration and average calculation
    Added functions to calculate average and register a student.

--- trabajo grupal.cpp
+++ trabajo grupal.cpp
@@ -13,6 +13,25 @@ struct Alumno {
 struct NodoLista { Alumno info; NodoLista* siguiente; };
 struct NodoCola  { Alumno info; NodoCola* siguiente; };
 struct NodoPila  { string accion; NodoPila* siguiente; };
+
+float calcularPromedio(float c1, float ep, float c2, float ef) {
+    return (c1 * 0.20) + (ep * 0.25) + (c2 + 0.20) + (ef * 0.35);
+}
+
+void registrarAlumno(NodoLista*& lista, int id, string nom, float n1, float n2, float n3, float n4) {
+    NodoLista* nuevo = new NodoLista();
+    nuevo->info.id = id;
+    nuevo->info.nombre = nom;
+    nuevo->info.c1 = n1;
+    nuevo->info.ep = n2;
+    nuevo->info.c2 = n3;
+    nuevo->info.ef = n4;
+    nuevo->info.promedio = calcularPromedio(n1, n2, n3, n4);
+    nuevo->info.estado = (nuevo->info.promedio >= 11) ? "Aprobado" : "Desaprobado";
+    nuevo->siguiente = lista;
+    lista = nuevo;
+}
+
 int main() {
 
     return 0;
