a97d7f870080bd508ac1b70a5dd46cbfabfa5be7
77916988-ctrl <77916988@continental.edu.pe>
2026-05-26 21:25:18 -0500

    descripcion de las lineas de codigo
    aun me faltan algunas por el momento las dejare

--- trabajo grupal.cpp
+++ trabajo grupal.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <iomanip>
+#include <iomanip> //Para darle formato a los numeros decimales
 
 using namespace std;
 
-struct Alumno {
+struct Alumno {  //implementamos la estructura principal
     int id;
     string nombre;
     float c1, ep, c2, ef;
@@ -13,17 +13,19 @@ struct Alumno {
     string estado;
 };
 
-struct NodoLista { Alumno info; NodoLista* siguiente; };
-struct NodoCola  { Alumno info; NodoCola* siguiente; };
-struct NodoPila  { string accion; NodoPila* siguiente; };
+struct NodoLista { Alumno info; NodoLista* siguiente; };  //los nodos aqui son como ladrillos oara construir por ejemplo aqui contiene el formulario alumno y
+struct NodoCola  { Alumno info; NodoCola* siguiente; };    // NodoLista*  un puntero que apunta al siguiente ladrillo o nodo 
+struct NodoPila  { string accion; NodoPila* siguiente; };   // nodo lista imaginemos qeue es un tre con bagones Nodo cola es como la fila de un banco NodoPila como si estuviesemos poniendo platos
+//pues asi cada uno recorre como debe ya sea cola FIFO o pila LIFO 
 
-float calcularPromedio(float c1, float ep, float c2, float ef) {
+float calcularPromedio(float c1, float ep, float c2, float ef) {   //esta es el calculo que se usara para sacar el promedio del alumno 
     return (c1 * 0.20) + (ep * 0.25) + (c2 * 0.20) + (ef * 0.35);
 }
 
-void registrarAlumno(NodoLista*& lista, int id, string nom, float n1, float n2, float n3, float n4) {
-    NodoLista* nuevo = new NodoLista();
-    nuevo->info.id = id;
+void registrarAlumno(NodoLista*& lista, int id, string nom, float n1, float n2, float n3, float n4) { //aqui creamos pero creamos alumno nuevo  y lo agrega al inicio de la lista 
+  
+    NodoLista* nuevo = new NodoLista(); //Crea un nuevo ladrillo (nodo) vacio en la memoria.
+    nuevo->info.id = id; //Llena todos los campos del alumno dentro del nuevo 
     nuevo->info.nombre = nom;
     nuevo->info.c1 = n1;
     nuevo->info.ep = n2;
@@ -36,9 +38,10 @@ void registrarAlumno(NodoLista*& lista, int id, string nom, float n1, float n2, 
 }
 
 void mostrarRegistro(NodoLista* lista) {
-    if (lista == NULL) { cout << "Registro vacio.\n"; return; }
-    NodoLista* temp = lista;
-    cout << "\nID\tNombre\tC1\tEP\tC2\tEF\tProm\tEstado\n";
+    
+    if (lista == NULL) { cout << "Registro vacio.\n"; return; } //Si la lista esta vacia (no hay ningun alumno), muestra un mensaje
+    NodoLista* temp = lista;        // Crea un puntero auxiliar "temp" que empieza apuntando al primer alumno. Usamos "temp" para no perder la referencia original "lista".
+    cout << "\nID\tNombre\tC1\tEP\tC2\tEF\tProm\tEstado\n"; // es el encabezado
     while (temp != NULL) {
         cout << temp->info.id << "\t" << temp->info.nombre << "\t" << temp->info.c1 << "\t"
              << temp->info.ep << "\t" << temp->info.c2 << "\t" << temp->info.ef << "\t"
@@ -47,7 +50,8 @@ void mostrarRegistro(NodoLista* lista) {
     }
 }
 
-void ordenarPorPromedio(NodoLista*& lista) {
+void ordenarPorPromedio(NodoLista*& lista) { //Muestra en pantalla TODOS los alumnos registrados con todos sus datos
+   
     if (lista == NULL) return;
     for (NodoLista* i = lista; i != NULL; i = i->siguiente) {
         for (NodoLista* j = i->siguiente; j != NULL; j = j->siguiente) {
